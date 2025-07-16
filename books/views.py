80c5584d58e359453ce4a34b52451c21020d93d6
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-16 10:48:57 +0530

    updated views.py

--- books/views.py
+++ books/views.py
@@ -2,6 +2,6 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
+class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
