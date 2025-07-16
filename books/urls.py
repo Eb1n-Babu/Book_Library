a926c1331917c6848da81d2c8cd4c86f4f388ca1
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-16 11:03:30 +0530

    updated urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import BookList
+from .views import BookListCreateAPIView
 
 urlpatterns = [
-    path('',BookList.as_view(),name='book-list'),
+    path('',BookListCreateAPIView.as_view(),name='book-list'),
 ]
